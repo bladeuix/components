7331788024d40d0c715dfa2284d8a06c81c6d2c6
Njogu Amos <29255728+njoguamos@users.noreply.github.com>
2026-04-18 15:24:22 +0300

    Remove unused views folder

--- src/Providers/DaisyUiServiceProvider.php
+++ src/Providers/DaisyUiServiceProvider.php
@@ -11,11 +11,6 @@ class DaisyUiServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        $this->loadViewsFrom(
-            path: __DIR__.'/../../resources/views',
-            namespace: 'daisyui'
-        );
-
         Blade::componentNamespace(
             namespace: 'BladeUix\\DaisyUi\\View\\Components',
             prefix: 'daisyui'
