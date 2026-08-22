1b1856db9a18530d4f2edc575df6b50fa3a67dea
Njogu Amos <29255728+njoguamos@users.noreply.github.com>
2026-08-22 21:55:18 +0300

    Add checkbox component

---
+++ tests/Feature/CheckboxTest.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BladeUix\DaisyUi\Tests\Feature;
+
+it(description: 'can render checkbox with default classes', closure: function () {
+    $view = $this->blade(template: '<x-daisyui::checkbox />');
+
+    $view->assertSee(value: '<input type="checkbox" class="checkbox">', escape: false);
+});
+
+it(description: 'can render checkbox with color and size classes', closure: function () {
+    $view = $this->blade(template: '<x-daisyui::checkbox color="primary" size="lg" />');
+
+    $view->assertSee(value: '<input type="checkbox" class="checkbox checkbox-lg checkbox-primary">', escape: false);
+});
+
+it(description: 'can render checkbox with checked and disabled attributes', closure: function () {
+    $view = $this->blade(template: '<x-daisyui::checkbox checked disabled />');
+
+    $view->assertSee(value: '<input type="checkbox" class="checkbox" checked="checked" disabled="disabled">', escape: false);
+});
+
+it(description: 'can render checkbox with a label', closure: function () {
+    $view = $this->blade(template: '<x-daisyui::checkbox label="Remember me" checked />');
+
+    $view->assertSee(value: '<label class="label"><input type="checkbox" class="checkbox" checked="checked"> Remember me</label>', escape: false);
+});
+
+it(description: 'can render checkbox with an empty label', closure: function () {
+    $view = $this->blade(template: '<x-daisyui::checkbox label="" />');
+
+    $view->assertSee(value: '<label class="label"><input type="checkbox" class="checkbox"> </label>', escape: false);
+});
+
+it(description: 'ignores unsupported checkbox color and size values', closure: function () {
+    $view = $this->blade(template: '<x-daisyui::checkbox color="invalid" size="invalid" />');
+
+    $view->assertSee(value: '<input type="checkbox" class="checkbox">', escape: false);
+});
+
+it(description: 'can render checkbox with custom attributes', closure: function () {
+    $view = $this->blade(template: '<x-daisyui::checkbox class="border-indigo-600" id="terms" />');
+
+    $view->assertSee(value: '<input type="checkbox" class="checkbox border-indigo-600" id="terms">', escape: false);
+});
