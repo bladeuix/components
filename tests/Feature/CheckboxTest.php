bf5d1ea87efebf306a1e84e5a3d9074336665bb9
Njogu Amos <29255728+njoguamos@users.noreply.github.com>
2026-08-23 00:20:03 +0300

    Remove label from checkbox

--- tests/Feature/CheckboxTest.php
+++ tests/Feature/CheckboxTest.php
@@ -26,22 +26,6 @@ it(description: 'does not allow custom type attributes to override checkbox type
     $view = $this->blade(template: '<x-daisyui::checkbox type="radio" />');
 
     $view->assertSee(value: '<input type="checkbox" class="checkbox">', escape: false);
-
-    $view = $this->blade(template: '<x-daisyui::checkbox label="Remember me" type="radio" />');
-
-    $view->assertSee(value: '<label class="label"><input type="checkbox" class="checkbox"> Remember me</label>', escape: false);
-});
-
-it(description: 'can render checkbox with a label', closure: function () {
-    $view = $this->blade(template: '<x-daisyui::checkbox label="Remember me" checked />');
-
-    $view->assertSee(value: '<label class="label"><input type="checkbox" class="checkbox" checked="checked"> Remember me</label>', escape: false);
-});
-
-it(description: 'can render checkbox with an empty label', closure: function () {
-    $view = $this->blade(template: '<x-daisyui::checkbox label="" />');
-
-    $view->assertSee(value: '<label class="label"><input type="checkbox" class="checkbox"> </label>', escape: false);
 });
 
 it(description: 'ignores unsupported checkbox color and size values', closure: function () {
